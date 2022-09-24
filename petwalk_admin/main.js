7e3d6ef3a80f73d79051272d1656ccd606be8d02
PhillipsMayfir <MachineTech2022@gmail.com>
2022-09-24 16:55:47 -0300

    commit actualización
    cambio tamaño de pantalla por defecto

--- petwalk_admin/main.js
+++ petwalk_admin/main.js
@@ -5,7 +5,7 @@ const path = require('path')
 function createWindow() {
     // Create the browser window.
     const mainWindow = new BrowserWindow({
-        width: 800,
+        width: 1200,
         height: 660,
         resizable: false,
         maximizable: false,
