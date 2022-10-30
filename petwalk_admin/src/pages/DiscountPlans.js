1237adb64fc3e70ad15208069ca8f643707f600e
PhillipsMayfir <MachineTech2022@gmail.com>
2022-10-29 23:10:56 -0300

    commit avances

--- petwalk_admin/src/pages/DiscountPlans.js
+++ petwalk_admin/src/pages/DiscountPlans.js
@@ -7,11 +7,16 @@ import { Table,Button,Input,notification,Modal,Form,message} from "antd";
 import TitleHeader from "../component/TitleHeader";
 import Icons from "../component/Icons";
 
-
+let idGlobal='';
 export default function DiscountPlans(){
 
 
     const [datos,setDatos]=useState([]);
+    const [modalDiscount,setModalDiscount]= useState(false);
+
+    const openCloseModalDiscount=()=>{
+        setModalDiscount(!modalDiscount);
+    }
 
 
     //Para consumir api de Node y listar
@@ -27,6 +32,26 @@ export default function DiscountPlans(){
         })
     }, [])
 
+    const [inputs,setInputs] = useState({
+        costo:'',
+        encabezado:'',
+    });
+
+    const changeForm = e =>{
+        setInputs({
+            ...inputs,
+            [e.target.name]:e.target.value
+
+        })
+    }
+
+    const selectionDiscount=(inputs,id)=>{
+        setInputs(inputs);
+        idGlobal = id;
+        openCloseModalDiscount()
+        return idGlobal;
+    } 
+
     const columns =[
         {
             title:'Costo',
@@ -39,10 +64,15 @@ export default function DiscountPlans(){
             key:'encabezado'
         },
         {
+            title:'Costo nuevo',
+            dataIndex:'costoNuevo',
+            key:'costoNuevo'
+        },
+        {
             title:'Agregar Descuento',
             dataIndex:'acciones',
             key:'acciones',
-            render: (fila,row) => <><Button type="primary" >Agregar Descuento</Button>
+            render: (fila,row) => <><Button type="primary" onAuxClick={()=> selectionDiscount(fila,row._id)} >Agregar Descuento</Button>
             </>
         },
         
@@ -56,6 +86,21 @@ export default function DiscountPlans(){
             </div>
             <TitleHeader></TitleHeader>
             <Table dataSource={datos} columns={columns}/>
+            <Modal
+            title="Añadir Descuento"
+            visible={modalDiscount}
+            destroyOnClose={true}
+            onCancel={openCloseModalDiscount}
+            centered
+            footer={[
+                <>
+                    <Button onClick={openCloseModalDiscount}>Cancelar</Button>
+                    <Button type="primary">Añadir Descuento</Button>
+                </>
+            ]}
+            >
+
+            </Modal>
         </div>
     )
 }
